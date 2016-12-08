0ac0ec9d785e42b3d9b5e838641a71eadd665b13
Donald Gray <donald.gray@maclean.co.uk>
2016-12-08 15:41:48 +0000

    Added overload of ThrowIfNull to take custom message

---
+++ src/Cake.Extensions.Tests/AssertExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+namespace Cake.Extensions.Tests
+{
+    using System;
+    using FluentAssertions;
+    using Xunit;
+
+    public class AssertExtensionTests
+    {
+        [Fact]
+        public void ThrowIfNull_WithMessage_SetsMessage()
+        {
+            object obj = null;
+
+            Action action = () => obj.ThrowIfNull(nameof(obj), "This is required");
+            action.ShouldThrow<ArgumentNullException>().WithMessage(
+            "This is required\r\nParameter name: obj");
+        }
+    }
+}
